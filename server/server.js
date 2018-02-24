7ea4a55cd8ec9f88221f6b55d140fe52273abd43
Yassine Chbani <yassinec@bam.tech>
2018-02-24 14:11:08 +0100

    Add test POST route

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('Hello Dev!');
+});
+
+app.post('/api/test/', function(req, res) {
+  res.json({ hey: req.body.first, yo: req.body.second });
 });
 
 app.listen(5000, function() {
