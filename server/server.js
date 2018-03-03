c73624be428d3674f1a6f30f7f31a2ba42f728d9
Yassine Chbani <yassinec@bam.tech>
2018-03-03 21:34:05 +0100

    Add API explorer

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const explorer = require('express-explorer');
+const bodyParser = require('body-parser');
 const secret = require('./secret.json');
-var stripe = require('stripe')(secret.stripeSecretKey);
+const stripe = require('stripe')(secret.stripeSecretKey);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,6 +15,12 @@ app.get('/', function(req, res) {
 app.post('/api/test/', function(req, res) {
   res.json({ hey: req.body.first, yo: req.body.second, vam: secret.test });
 });
+
+const settings = {
+  format: 'html',
+};
+
+app.use('/explorer', explorer(settings));
 
 app.listen(5000, function() {
   console.log('Dev app listening on port 5000!');
