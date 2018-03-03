c8abce6d5d7f6dac45284af8f05248d4cfb3a6b4
Yassine Chbani <yassinec@bam.tech>
2018-03-03 20:45:49 +0100

    Add paymentRequestWithCardForm

--- DollarzApp/App.js
+++ DollarzApp/App.js
@@ -5,21 +5,46 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import stripe from 'tipsi-stripe';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
+stripe.init({
+  publishableKey: 'pk_test_NXzesZUopyI0RM7xO4HoIEg3',
+});
+
 type Props = {};
-export default class App extends Component<Props> {
+
+type State = {
+  token: { livemode: boolean, created: number, card: Object, tokenId: string },
+};
+
+export default class App extends Component<Props, State> {
+  constructor() {
+    super();
+    this.state = {
+      token: { tokenId: '' },
+    };
+  }
+
+  requestPayment = () => {
+    stripe.paymentRequestWithCardForm().then(stripeTokenInfo => {
+      this.setState({ token: stripeTokenInfo });
+    });
+  };
+
   render() {
+    console.log(this.state.token);
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Button title={'Entrer carte'} style={styles.payment} onPress={this.requestPayment} />
+        <Text>{this.state.token.tokenId}</Text>
       </View>
     );
   }
@@ -37,9 +62,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  payment: {
+    padding: 16,
+    borderWidth: 1,
+    borderColor: 'black',
   },
 });
